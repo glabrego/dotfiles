4f1ef64c50c59b539960f60b8ba7334f1eb11eb1
Guilherme Labrego <glabrego@gmail.com>
2017-12-18 14:42:44 -0200

    Fix symlink of tmux configs

--- setup.sh
+++ setup.sh
@@ -58,7 +58,7 @@ ln -s ~/workspace/dotfiles/.aliases   ~/.aliases
 ln -s ~/workspace/dotfiles/.functions ~/.functions
 ln -s ~/workspace/dotfiles/.zshrc     ~/.zshrc
 ln -s ~/workspace/dotfiles/.vimrc     ~/.vimrc
-ln -s ~/workspace/dotfiles/.vimrc     ~/.tmux.conf
+ln -s ~/workspace/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/workspace/dotfiles/.spacemacs ~/.spacemacs
 echo 'Done!'
 
